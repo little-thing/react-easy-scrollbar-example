1896468daa9a52d25746ba810e9fdccaec018a6b
常威 <changwei@dtstack.com>
2024-07-14 23:41:11 +0800

    fix: examples app2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as path from 'node:path';
+
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'react-easy-scrollbar': path.resolve(process.cwd(), '../'),
-    },
-  },
 })
